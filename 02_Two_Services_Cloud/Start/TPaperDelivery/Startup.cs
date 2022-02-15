ec79f202b8a42110f947291ab295697f346c8396
Stas Lebedenko <Stas Lebedenko>
2022-02-15 22:54:29 +0200

    Step 2 Start initial commit

---
+++ 02_Two_Services_Cloud/Start/TPaperDelivery/Startup.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace TPaperDelivery
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging(options =>
+            {
+                options.AddFilter("TPaperDelivery", LogLevel.Information);
+            });
+
+            services.AddOptions<ProjectOptions>()
+                .Configure<IConfiguration>((settings, configuration) =>
+                {
+                    configuration.GetSection("ProjectOptions").Bind(settings);
+                });
+
+            services.AddControllers();
+            services.AddHttpClient();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ 02_Two_Services_Cloud/Start/TPaperOrders/Controllers/OrderController.cs
@@ -0,0 +1,64 @@
+using Dapr.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TPaperOrders
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController
+    {
+        private readonly ILogger<OrderController> _logger;
+
+        private readonly IHttpClientFactory _clientFactory;
+
+        private readonly DaprClient _daprClient;
+
+        public OrderController(
+            ILogger<OrderController> logger,
+            IHttpClientFactory clientFactory,
+            DaprClient daprClient)
+        {
+            _logger = logger;
+            _clientFactory = clientFactory;
+            _daprClient = daprClient ?? throw new ArgumentNullException(nameof(daprClient));
+        }
+
+        [HttpGet]
+        [Route("create/{quantity}")]
+        public async Task<IActionResult> ProcessEdiOrder(decimal quantity, CancellationToken cts)
+        {
+            _logger.LogInformation("Processed a request.");
+
+            var order = new EdiOrder
+            {
+                ClientId = 1,
+                DeliveryId = 1,
+                Notes = "Test order",
+                ProductCode = 1,
+                Quantity = quantity
+            };
+
+            DeliveryModel savedDelivery = await CreateDeliveryForOrder(order, cts);
+
+            string responseMessage = $"Accepted EDI message {order.Id} and created delivery {savedDelivery?.Id}";
+
+            return new OkObjectResult(responseMessage);
+        }
+
+        private async Task<DeliveryModel> CreateDeliveryForOrder(EdiOrder savedOrder, CancellationToken cts)
+        {
+
+            var route = $"api/delivery/create/{savedOrder.ClientId}/{savedOrder.Id}/{savedOrder.ProductCode}/{savedOrder.Quantity}";
+
+            DeliveryModel savedDelivery = await _daprClient.InvokeMethodAsync<DeliveryModel>(
+                HttpMethod.Get, "tpaperdelivery", route, cts);
+
+            return savedDelivery;
+        }
+    }
+}
